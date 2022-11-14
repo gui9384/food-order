55dee0b295ab9dcd301213149738944d24a9f18d
Guillaume <g.debouard@gmail.com>
2022-11-14 21:58:28 +0800

    added cart modal

--- src/Components/Meals/MealsSummary.js
+++ src/Components/Meals/MealsSummary.js
@@ -4,7 +4,8 @@ const MealsSummary = () => {
   return (
     <section className={classes.summary}>
       <h2>Delicious Food, Delivered To You</h2>
-      <p>
+      <p style={{color:'green'}}>
+        The second feature is implemented
         Choose your favorite meal from our broad selection of available meals
         and enjoy a delicious lunch or dinner at home.
       </p>
